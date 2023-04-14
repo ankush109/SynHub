81dbfb1a46f3f0190122ea65516c6acaa641b7a7
Souvik <souviksen3469@gmail.com>
2023-04-14 21:26:27 +0530

    Navbar 1

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,22 +1,22 @@
-import AppBar from '@/components/AppBar'
-import Home from '@/components/Home'
-import LeftBar from '@/components/LeftBar'
-import Rightbar from '@/components/Rightbar'
-import React from 'react'
+import AppBar from "@/components/AppBar";
+import Home from "@/components/Home";
+import LeftBar from "@/components/LeftBar";
+import Rightbar from "@/components/Rightbar";
+import React from "react";
 
 function index() {
   return (
-  <div className="">
+    <div className="">
       <div className="">
         <AppBar />
         <div className="flex flex-row h-[88.5vh]">
-          <LeftBar/>
+          <LeftBar />
           <Home />
-       <Rightbar/>
+          <Rightbar />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default index
+export default index;
